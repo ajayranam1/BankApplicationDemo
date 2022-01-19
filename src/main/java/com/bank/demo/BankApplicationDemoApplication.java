09767a41a2abb377f6ac31c2997516928972ba17
Jaiprakash Mishra <mjaiprakash469@gmail.com>
2022-01-19 17:35:59 +0530

    Created new project

---
+++ src/main/java/com/bank/demo/BankApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bank.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationDemoApplication.class, args);
+	}
+
+}
